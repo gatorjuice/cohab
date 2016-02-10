5ce92ccc62baa66c89826e117a2af65bbf851500
gatorjuice <gatorjuice@gmail.com>
2016-02-10 15:47:45 -0600

    added math to show difference in spending summery

--- app/controllers/partnerships_controller.rb
+++ app/controllers/partnerships_controller.rb
@@ -7,7 +7,7 @@ class PartnershipsController < ApplicationController
 	def create
 		@partnership = Partnership.new(
 			name: params[:name]
-		)
+			)
 		if @partnership.save
 			redirect_to "/partnerships/#{@partnership.id}"
 		else
@@ -19,6 +19,25 @@ class PartnershipsController < ApplicationController
 	def show
 		@partnership = Partnership.find(params[:id])
 		@expenses = Expense.where(partnership_id: @partnership.id).order(:created_at).reverse
+		@grouped_expenses = @expenses.group_by {|expense| expense.user.firstname }
+
+		high_spender = @grouped_expenses.max_by { |k, v| v }
+		low_spender = @grouped_expenses.min_by { |k, v| v }
+
+		if @expenses.length > 0
+			high_spender_total = 0
+			high_spender[1].each do |expense|
+				high_spender_total += expense.value
+			end
+			low_spender_total = 0
+			low_spender[1].each do |expense|
+				low_spender_total += expense.value
+			end
+
+			difference = high_spender_total - low_spender_total
+			
+			@summary_statement = "#{high_spender[0]} has outspent #{low_spender[0]} by $#{difference}"
+		end
 	end
 
 
@@ -29,14 +48,14 @@ class PartnershipsController < ApplicationController
 			firstname: params[:firstname],
 			lastname: params[:lastname],
 			email: params[:email]
-		)
+			)
 		@user.password = params[:password]
 		@user.save!
 
 		@user_partnership = UserPartnership.new(
-		  user_id: @user.id,
-		  partnership_id: params[:partnership]
-		)
+			user_id: @user.id,
+			partnership_id: params[:partnership]
+			)
 		@user_partnership.save
 		redirect_to "/partnerships/#{params[:partnership]}"
 	end
