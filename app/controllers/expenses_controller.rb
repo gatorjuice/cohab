27f068dec0d308daf35ca3d797a38f74eeb03d9d
gatorjuice <gatorjuice@gmail.com>
2016-02-10 22:24:40 -0600

    fixed math on differnce in contributions

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,15 +2,15 @@ class ExpensesController < ApplicationController
 
 	def create
 		expense_description = params[:expense_description]
-		price = expense_description.match(/\d+(?:[.,]\d+)?/).to_s
-		description = expense_description.delete(price).strip
+		price = expense_description.match(/\d+(?:[.,]\d+)?/).to_s.to_d
+		description = expense_description.delete(price.to_s).strip
 		@expense = Expense.new(
 			user_id: params[:user],
 			partnership_id: params[:partnership],
 			value: price,
 			description: description
 		)
-		puts price
+		p price
 		puts description
 		@expense.save
 		redirect_to "/partnerships/#{params[:partnership]}"
--- app/controllers/partnerships_controller.rb
+++ app/controllers/partnerships_controller.rb
@@ -19,24 +19,29 @@ class PartnershipsController < ApplicationController
 	def show
 		@partnership = Partnership.find(params[:id])
 		@expenses = Expense.where(partnership_id: @partnership.id).order(:created_at).reverse
-		@grouped_expenses = @expenses.group_by {|expense| expense.user.firstname }
 
+		puts "+++++++++++++++++++"
+		@grouped_expenses = {}
+
+		@expenses.each {|expense| @grouped_expenses[expense.user.firstname] = 0}
+
+		@expenses.each do |expense|
+			@grouped_expenses[expense.user.firstname] += expense.value
+		end
+
+		puts @grouped_expenses
 		high_spender = @grouped_expenses.max_by { |k, v| v }
 		low_spender = @grouped_expenses.min_by { |k, v| v }
-
+		
+		puts high_spender
+		puts low_spender
+		
 		if @expenses.length > 0
-			high_spender_total = 0
-			high_spender[1].each do |expense|
-				high_spender_total += expense.value
-			end
-			low_spender_total = 0
-			low_spender[1].each do |expense|
-				low_spender_total += expense.value
-			end
 
-			difference = high_spender_total - low_spender_total
-			
+			difference = high_spender[1] - low_spender[1]
+
 			@summary_statement = "#{high_spender[0]} has outspent #{low_spender[0]} by $#{difference}"
+		
 		end
 	end
 
@@ -45,8 +50,8 @@ class PartnershipsController < ApplicationController
 
 	def add_member
 		@user = User.new(
-			firstname: params[:firstname],
-			lastname: params[:lastname],
+			firstname: params[:firstname].capitalize,
+			lastname: params[:lastname].capitalize,
 			email: params[:email]
 			)
 		@user.password = params[:password]
